0af8b200aba1b50454a21b621dad744fc8a35777
Jose Adiel Baez <joseadielbaez78@gmail.com>
2026-08-19 23:01:39 -0400

    Al mover el ratón, cada cuadrado de la cuadrícula se pinta de un color.

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ div.classList.add('container');
 document.body.appendChild(div);
 
 for (let i = 0; i < 256; i++) {
+    // Cuadricula 16 x 16
     const square = document.createElement('div');
     square.classList.add('square');
     const container = document.querySelector('.container');
     container.appendChild(square);
+    // Eventos del ratón
+    square.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = 'red';
+    });
 }
